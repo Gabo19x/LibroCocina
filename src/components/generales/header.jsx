ec606c4bc67ee9fb7410f7badf1a6d650820ef6a
Gabo19 <arturopinzon19@gmail.com>
2026-07-11 16:21:32 -0500

    Mejora al header para mostrar botones segun la sesion

--- src/components/generales/header.jsx
+++ src/components/generales/header.jsx
@@ -1,9 +1,34 @@
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/Autenticar';
 
 import "../../styles/header/header.css";
 
 export default function Header({principal}) {
     const navegar = useNavigate();
+    const { user, signOut } = useAuth();
+
+    async function CerrarSesion() {
+        await signOut();
+        navegar('/');
+    }
+
+    function BotonesCuenta() {
+        if(user) {
+            return(
+                <div className="BotonesCuenta">
+                    <button className='Boton_menu' onClick={() => {navegar("/cuenta")}}>👨🏽‍🍳 Mi cuenta</button>
+                    <button className='Boton_menu' onClick={() => {CerrarSesion()}}>❌ Cerrar sesion</button>
+                </div>
+            );
+        } else {
+            return (
+                <div className="BotonesCuenta">
+                    <button className='Boton_menu' onClick={() => {navegar("/registro")}}>👨🏽‍🍳 Crear cuenta</button>
+                    <button className='Boton_menu' onClick={() => {navegar("/login")}}>🍖 Iniciar sesion</button>
+                </div>
+            );
+        }
+    }
 
     if(principal) {
         return (
@@ -11,8 +36,7 @@ export default function Header({principal}) {
                 <h3>Mi libro de cocina:</h3>
                 <h1>El jardin culinario</h1>
                 <hr />
-                <button className='Boton_menu' onClick={() => {navegar("/registro")}}>👨🏽‍🍳 Crear cuenta</button>
-                <button className='Boton_menu' onClick={() => {navegar("/login")}}>🍖 Iniciar sesion</button>
+                <BotonesCuenta />
                 <button className='Boton_menu' onClick={() => {navegar("/busqueda-avanzada")}}>Busqueda avanzada</button>
             </header>
         );
