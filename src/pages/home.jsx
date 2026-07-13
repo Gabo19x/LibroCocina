32eda6ddb4e9b5181a713d3c388140e6dda798ef
Gabo19 <arturopinzon19@gmail.com>
2026-07-13 16:40:48 -0500

    Implementacion en el home de la receta destacada o mas nueva

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+import { supabase } from '../supabaseClient';
 import UseApi from "../hooks/useApi";
 import Header from "../components/generales/header";
 import Footer from "../components/generales/footer";
@@ -13,13 +14,37 @@ import "../styles/home/home.css";
 
 export default function Home() {
     const {data, cargando} = UseApi();
+    const [recetaNueva, setRecetaNueva] = useState(null);
+
+    /* FUNCION de flecha
+        Con los datos de la API, filtra las recetas evitando la mas reciente,
+        luego las mapea como componentes para ser usadas.
+    */
+    const listaRecetas = data
+        .filter((receta) => receta.id !== recetaNueva.id)
+        .map((receta) => (
+            <Comida 
+                tipo={true}
+                comida={receta}
+            />
+        )
+    )
     
-    const listaObjetos = data.map((record) => (
-        <Comida 
-            tipo={true}
-            comida={record}
-        />
-    ));
+    /* USEEFFECT
+        Se ejecuta al inicio del renderizado, 
+        y con una funcion asincrona: obtiene la receta mas nueva de supabase y se guarda.
+    */
+    useEffect(() => {
+        async function ComidaNueva() {
+            const {data: reciente} = await supabase
+                .from("recipes").select("*").order("created_at", {ascending: false}).limit(1).single()
+
+            setRecetaNueva(reciente);
+        }
+
+        ComidaNueva()
+    }, [])
+    
 
     const { user, profile, signOut, isAdmin } = useAuth()
     
@@ -40,13 +65,23 @@ export default function Home() {
                     <Buscador/>
 
                     <div className="Paginacion">
+                        <h2>⭐ Receta recién "horneada"</h2>
+
+                        <section className="Paginacion_flex">
+                            { 
+                                ( cargando )
+                                ? <h3 className="Mensaje_cargando">🥪 Cargando comida destacada...</h3>
+                                : <Comida tipo={true} comida={recetaNueva} />
+                            }
+                        </section>
+
                         <h2>Recetas publicadas</h2>
 
                         <section className="Paginacion_flex">
                             { 
                                 ( cargando )
                                 ? <h3 className="Mensaje_cargando">🥪 Cargando comida...</h3>
-                                : <Paginacion elementos={listaObjetos} />
+                                : <Paginacion elementos={listaRecetas} />
                             }
                         </section>
                     </div>
